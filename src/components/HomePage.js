39c63a494de27ab9e0514a07f027fce3c2f76777
piyush3441 <piyushsolanki607@gmail.com>
2023-03-14 23:51:42 +0530

    new card between comments

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -91,6 +91,21 @@ const HomePage = () => {
                   </Col>
 
                   {/* Card Number 2 */}
+                  <Col className="col-1" xs={12} sm={6} md={6} lg={4}>
+                      <center>
+                          <Card style={{ width: '30rem', height: '35rem' }}>
+                              <Card.Img variant="center" src="food.jpg" />
+                              <Card.Body>
+                                  <Card.Title>12345 Numbers</Card.Title>
+                                  <Card.Text>
+                                      Some quick example text to build on the card title and make up the
+                                      bulk of the card's content.
+                                  </Card.Text>
+                                  <Button className="card-btn" variant="primary">Go somewhere</Button>
+                              </Card.Body>
+                          </Card>
+                      </center>
+                  </Col>
                   {/* ?? */}
                   {/* Hello There it is another card */}
                   <Col className="col-1" xs={12} sm={6} md={6} lg={4}>
