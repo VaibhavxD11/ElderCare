4325ebbf8cc28fe187179b3f357d7b5ec57af3b9
VaibhavxD11 <dubeyvaibhav16@gmail.com>
2023-03-14 23:04:05 +0530

    About & Contact Form CSS enabled

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
-// import './About.css';
+import './About.css';
 
 function About() {
     return (
