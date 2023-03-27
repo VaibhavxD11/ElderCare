218d5f3c3ff703146c4820a7e8bdd347f539f50d
piyush3441 <piyushsolanki607@gmail.com>
2023-03-27 19:55:31 +0530

    Signup page made

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,75 +1,70 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css';
-{/* <script>querySelector
-        document.('.img__btn').addEventListener('click', function() {
-            document.querySelector('.cont').classList.toggle('s--signup');
-        });
-    </script> */}
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 const Login = () => {
-    const [toggle, setToggle] = useState(false)
-    return (
-
-        <div className="login-form">
-            <div class={`cont ${toggle ? "s--signup" : ""}`}>
-                <div class="loginform sign-in">
-                    <h2>Login as Elder</h2>
-                    <label>
-                        <span>id</span>
-                        <input type="integer" />
-                    </label>
-                    {/* <label>
-                    <span>Password</span>
-                    <input type="password" />
-                </label> */}
-                    {/* <p class="forgot-pass">Forgot password?</p> */}
-                    <button type="button" class="submit">Sign In</button>
-
-                </div>
-                <div class="sub-cont">
-                    <div class="img">
-                        <div class="img__text m--up">
-
-                            <h2>Guardian? </h2>
-                            <h3>Want to login?</h3>
-                        </div>
-                        <div class="img__text m--in">
-                            <h3>Have account as Elder? </h3>
-                            <h3>Want to login?</h3>
-                        </div>
-                        <div class="img__btn" onClick={() => {
-                            setToggle(!toggle)
-                        }}>
-                            <span class="m--up">Sign In</span>
-                            <span class="m--in">Sign In</span>
-                        </div>
-                    </div>
-                    <div class="loginform sign-up">
-                        <h2>Login as Guardian</h2>
-                        <label>
-                            <span>Guardian Name</span>
-                            <input type="text" />
-                        </label>
-                        <label>
-                            <span>Elder Name</span>
-                            <input type="text" />
-                        </label>
-                        <label>
-                            <span>Password</span>
-                            <input type="password" />
-                        </label>
-                        <button type="button" class="submit">Sign In</button>
-                        <div class="signup-link">Doesnot have an account? <Link class="no-account" to={"/signup"}>Signup</Link>
-                        </div>
-
-
-                    </div>
-                </div>
-            </div>
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div className="login-form">
+      <div class={`cont ${toggle ? "s--signup" : ""}`}>
+        <div class="loginform sign-in">
+          <h2>Login as Elder</h2>
+          <label>
+            <span>id</span>
+            <input type="integer" />
+          </label>
+          <button type="button" class="submit">
+            Sign In
+          </button>
         </div>
-
-    )
-}
-
+        <div class="sub-cont">
+          <div class="img">
+            <div class="img__text m--up">
+              <h2>Guardian? </h2>
+              <h3>Want to login?</h3>
+            </div>
+            <div class="img__text m--in">
+              <h3>Have account as Elder? </h3>
+              <h3>Want to login?</h3>
+            </div>
+            <div
+              class="img__btn"
+              onClick={() => {
+                setToggle(!toggle);
+              }}
+            >
+              <span class="m--up">Sign In</span>
+              <span class="m--in">Sign In</span>
+            </div>
+          </div>
+          <div class="loginform sign-up">
+            <h2>Login as Guardian</h2>
+            <label>
+              <span>Guardian Name</span>
+              <input type="text" />
+            </label>
+            <label>
+              <span>Elder Name</span>
+              <input type="text" />
+            </label>
+            <label>
+              <span>Password</span>
+              <input type="password" />
+            </label>
+            <button type="button" class="submit">
+              Sign In
+            </button>
+            <div class="signup-link">
+              Doesnot have an account?{" "}
+              <Link class="no-account" to={"/signup"}>
+                Signup
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,15 +1,55 @@
-import React from 'react';
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Signup.css";
 
 const Signup = () => {
-    return (
-        <section className="hero-section">
-            <a href="/"><img src="image.jpg" height="320px" width="300px"></img></a>
-            <h1>Sign Up</h1>
-        </section>
-    )
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div className="signup-form">
+      <div class={`cont ${toggle ? "s--signup" : ""}`}>
+        <div class="loginform sign-in">
+          <h2>Signup for Guardian</h2>
+          <label>
+            <span>Guardian Name</span>
+            <input type="text" />
+          </label>
+          <label>
+            <span>Elder Name</span>
+            <input type="text" />
+          </label>
+          <label>
+            <span>Guardian Address</span>
+            <input type="text" />
+          </label>
+          <label>
+            <span>Password</span>
+            <input type="text" />
+          </label>
+          <button type="button" class="submit">
+            Sign up
+          </button>
+        </div>
+        <div class="sub-cont">
+          <div class="img">
+            <div class="signup__text m--up">
+              <h3>Signup</h3>
+              <h3>for Guardian</h3>
+              <div class="signup-link">
+                Have an account?{" "}
+                <Link
+                  class="no-account"
+                  to={"/Login"}
+                  style={{ color: "black" }}
+                >
+                    <u>Login as Guardian</u>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
-
